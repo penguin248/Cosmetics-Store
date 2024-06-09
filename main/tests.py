37c89bce001ccaed6c544417337a202985141e06
James_Matara <jamesmatara11@gmail.com>
2024-06-09 15:50:54 +0300

    Add test to check if the home page works

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestPage (TestCase):
+    def test_home_page_works (self):
+        """Test to see if the home page works"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'Cosmetics')
+
+
