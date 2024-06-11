ee7dff275b7f3b20cbe52a9a43453474d353024f
James_Matara <jamesmatara11@gmail.com>
2024-06-11 12:46:50 +0300

    Update contact form

--- main/forms.py
+++ main/forms.py
@@ -5,10 +5,23 @@ import logging
 logger = logging.getLogger(__name__)
 
 class ContactForm(forms.Form):
-    names = forms.CharField(label='Your name', max_length=100)
-    message = forms.CharField(max_length=600, widget=forms.Textarea)
-    
+    name = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
     def send_mail(self):
         logger.info("Sending email to customer service")
-        message = "From: {0}\n{1}".format(self.cleaned_data["name"], self.cleaned_data["message"])
-        send_mail("site message", message, "site@cosmetics.domain", ["customerservice@cosmetics.domain"], fail_silently=False,)
+        
+        # Debugging: Print out cleaned data
+        logger.debug("Cleaned data: %s", self.cleaned_data)
+        
+        name = self.cleaned_data.get("name", "Unknown")
+        message_content = self.cleaned_data.get("message", "No message provided.")
+        message = "From: {0}\n{1}".format(name, message_content)
+        
+        send_mail(
+            "site message",
+            message,
+            "site@cosmetics.domain",
+            ["customerservice@cosmetics.domain"],
+            fail_silently=False,
+        )
