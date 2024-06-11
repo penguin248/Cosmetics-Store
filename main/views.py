7da6d42c8d8429e5bb92b48ee7fe571251024884
James_Matara <jamesmatara11@gmail.com>
2024-06-11 12:44:19 +0300

    Update FormView to correctly handle form submissions

--- main/views.py
+++ main/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
 from django.views.generic.edit import FormView
-from main import forms
+from .forms import ContactForm
+import logging
 
-class ContactUsView (FormView):
+logger = logging.getLogger(__name__)
+
+class ContactUsView(FormView):
     template_name = "contact_form.html"
-    form_class = forms.ContactForm
+    form_class = ContactForm
     success_url = "/"
     
     def form_valid(self, form):
+        logger.debug("Form is valid")
         form.send_mail()
         return super().form_valid(form)
+
+    def form_invalid(self, form):
+        logger.warning("Form is invalid: %s", form.errors)
+        return super().form_invalid(form)
+    
+    def post(self, request, *args, **kwargs):
+        logger.debug("POST data: %s", request.POST)
+        return super().post(request, *args, **kwargs)
+
 
 # Create your views here.
