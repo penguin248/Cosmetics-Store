e6755209c83ab7206f8cb733bc62ece0ef0c94d2
James_Matara <jamesmatara11@gmail.com>
2024-06-11 08:15:54 +0300

    Add and update about us and home page tests Add and update about us and home page tests to include the reverse function

--- main/tests.py
+++ main/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class TestPage (TestCase):
     def test_home_page_works (self):
         """Test to see if the home page works"""
-        response = self.client.get("/")
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'Cosmetics')
+        
+    def test_home_page_works (self):
+        """Test to see if the about page works"""
+        response = self.client.get(reverse("about_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about_us.html')
+        self.assertContains(response, 'Cosmetics')
+        
+
 
 
