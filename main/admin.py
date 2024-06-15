82379e659ecce530e68d5570a666a71476eecaba
James_Matara <jamesmatara11@gmail.com>
2024-06-15 16:41:23 +0300

    Register models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# Register models
+admin.site.register(models.Product)
+admin.site.register(models.ProductTag)
+admin.site.register(models.ProductImage)
